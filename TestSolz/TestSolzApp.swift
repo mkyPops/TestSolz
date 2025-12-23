fba58b08d02c3c5771f19dbaec98b8320fac997f
mkyPops <gamingmky5@gmail.com>
2025-12-23 17:33:18 +0500

    Initial Commit

---
+++ TestSolz/TestSolzApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSolzApp.swift
+//  TestSolz
+//
+//  Created by Mashaal Khan on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestSolzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
